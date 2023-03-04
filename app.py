fa712d3f70b51a33b3ca77fe2d11d32701a8cf10
MTurjotarifica <116021103+MTurjotarifica@users.noreply.github.com>
2023-03-05 04:20:47 +0800

    Update app.py

--- app.py
+++ app.py
@@ -160,6 +160,92 @@ def mp3_trigger():
     return f'{greeting_message}', 200
 
 
+#######################################
+#background worker for deep l where only file is provided
+def backgroundworker_deepl_text_lang(text_lang_to, text_to_translate, response_url,channel_id):
+
+    # your task
+
+    # DeepL auth key is stored in environment variable which we are obtaining
+    translator = deepl.Translator(os.environ.get('DEEPL_AUTH_KEY'))
+
+    #using text argument to translate text to Specified language
+    result = translator.translate_text(f'{text_to_translate}', 
+                                       target_lang=f'{text_lang_to}') 
+
+    #storing translated in a variable
+    translated_text = result.text
+
+
+
+    #payload is required to to send second message after task is completed
+    payload = {"text":"your task is complete",
+                "username": "bot"}
+
+    #posting translated text to slack channel
+    client.chat_postMessage(channel=channel_id,
+                            text=f"{translated_text}"
+                            )
+
+
+    requests.post(response_url,data=json.dumps(payload))
+    
+
+#deepl trigger slash command which creates translation for speech blocks 
+#and posts to slack
+@app.route('/deepl_trigger_with_lang', methods=['POST'])
+def deepl_trigger_with_lang():
+    data = request.form
+    channel_id = data.get('channel_id')
+    #we are usging data2 to parse the information
+    data2 = request.form.to_dict()
+    #print(data)
+    user_id = data.get('user_id')
+    channel_id = data.get('channel_id')
+    #getting language to translate to
+    text_lang_to = data.get('text').split()[0]
+    #text to translate (we are taking portions after en-gb/en-us etc)
+    if (text_lang_to.lower() == 'en-gb'):
+        text_to_translate = data.get('text')[6:]
+    elif (text_lang_to.lower() == 'en-us'):
+        text_to_translate = data.get('text')[6:]
+    elif (text_lang_to.lower() == 'pt-pt'):
+        text_to_translate = data.get('text')[6:]
+    elif (text_lang_to.lower() == 'pt-br'):
+        text_to_translate = data.get('text')[6:]
+    else:
+        text_to_translate = data.get('text')[3:]
+
+    response_url = data.get("response_url")
+    #event = payload.get('event', {})
+    #text = event.get('text')
+    greeting_message = "Processing your request. Please wait."
+    ending_message = "Process executed successfully"
+
+
+    client.chat_postMessage(channel=channel_id,
+                            text="DeepL Translation loading. Please wait."
+                            )
+
+
+    #triggering backgroundworker for deepl with arguments lang to translate from
+    #translate to and text to translate
+    thr = Thread(target=backgroundworker_deepl_text_lang, 
+                 args=[text_lang_to,
+                       text_to_translate,
+                       response_url,channel_id
+                      ]
+                 )
+
+    thr.start()
+
+
+    #returning empty string with 200 response
+    return f'{greeting_message}', 200
+
+#######################################
+
+
 # Define the function that handles the /example command
 def handle_example_command(text):
     return "You entered: {}".format(text)
