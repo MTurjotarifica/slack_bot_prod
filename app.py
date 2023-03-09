0d105b058da04f459e7de78baf00cb7a47f3bd38
MTurjotarifica <116021103+MTurjotarifica@users.noreply.github.com>
2023-03-09 14:31:08 -0500

    Update app.py
    added load df lines

--- app.py
+++ app.py
@@ -56,6 +56,43 @@ client = slack_app.client
 
 
 ##################################################________________________________________
+#function to create load difital demand as dataframe
+def load_dd_df():
+    '''
+    loads digital demand dataframe for all dates
+    where country is DE
+    and gt_category 13
+    returns:
+        df_dd_raw (Dataframe)
+    '''    
+    #SQL ALCHEMY
+    #creating the engine
+    #syntax: dialect+driver://username:password@host:port/database
+    engine = create_engine('mysql+pymysql://sandbox_read_only:zhsqehk23Xs8tVmVn3sSkyq5TvZumR5q@mysqldatabase.cmi5f1vp8ktf.us-east-1.rds.amazonaws.com:3306/sandbox')
+    
+    #creating a connection object
+    connection = engine.connect()
+    
+    #creating the metadata object
+    metadata = MetaData()
+    
+    #loading the digital_demand table #edit pik 2023-03-07
+    df_dd_raw_table = Table('digital_demand',
+                            metadata
+			   )
+    
+    #this is the query to be performed #edit st 2023-03-07
+    stmt = "SELECT * FROM digital_demand WHERE (gt_category = 13) AND (country = 'DE');"
+    
+    df_dd_raw = pd.read_sql(sqlalctext(stmt), connection) #edit st 2023-03-07
+    df_dd_raw['date'] = pd.to_datetime(df_dd_raw['date'])
+    
+    connection.close()
+    
+    return df_dd_raw
+
+#storing df_digital_demand in variable df_raw to maintain code in viz generator
+df_raw = load_dd_df()
 
 #################################################
 
@@ -74,12 +111,16 @@ condition_list_dd_vis = []
 
 #######################################----------------------------------------------
 
+
+
 ################################################
 # st edit mar 08, 2023 fixed indentation using vs code
 # indendation errors are being caused by spyder (the ide I use)
 
 
 ################################################-------------------------------------------
+
+
 
 #######################################################__________________________________
 
@@ -89,6 +130,28 @@ def handle_example_command(text):
 
 
 # Define the function that handles the /hello command
+# Handle incoming slash command requests
+@app.route("/slack/command", methods=["POST"])
+def handle_slash_command():
+    # Parse the command and its parameters from the request
+    command = request.form.get("command")
+    text = request.form.get("text")
+    channel_id = request.form.get("channel_id")
+
+    # Execute the appropriate function based on the command
+    if command == "/example":
+        client.chat_postMessage(channel='#slack_bot_prod', text=f"it worksssss! {df_raw.date.max()}")
+        response_text = handle_example_command(text)
+    else:
+        response_text = "Unknown command: {}".format(command)
+
+    # Send the response to the channel
+#     slack_app.client.chat_postMessage(channel=response_url, text=response_text)
+
+
+    # Return an empty response to Slack
+    return make_response("", 200)
+
 
 # Add a route for the /hello command
 @app.route("/hello", methods=["POST"])
