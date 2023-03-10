32cc4f74225b914b7229b31fbf6977d75ea8be57
MTurjotarifica <116021103+MTurjotarifica@users.noreply.github.com>
2023-03-10 12:05:21 -0500

    Update app.py
    changed test slash command

--- app.py
+++ app.py
@@ -740,7 +740,7 @@ def handle_hello_request():
     channel_id = data.get('channel_id')
     # Execute the /hello command function
     slack_app.client.chat_postMessage(response_type= "in_channel", channel=channel_id, text="it works!", )
-    client.chat_postMessage(response_type= "in_channel", channel='#slack_bot_prod', text=" 2nd it works!2!", )
+    client.chat_postMessage(response_type= "in_channel", channel='#slack_bot_prod', text=" 2nd it works!33!", )
     return "Hello world1" , 200
 
 
