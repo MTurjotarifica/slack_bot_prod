176aed213b8d2676c457f17d4ea0a44cf3e01891
MTurjotarifica <116021103+MTurjotarifica@users.noreply.github.com>
2023-03-15 06:06:38 -0400

    Update app.py
    removed os.expand user for gdelt and added wiki_csv trigger

--- app.py
+++ app.py
@@ -36,12 +36,16 @@ from plotly.subplots import make_subplots
 import plotly.express as px
 import matplotlib.dates as mdates
 import kaleido # required for fig.write in azure
-import scipy.signal
+import scipy.signal #used in dd_vis generation
 
 import slack
 
+# for wiki_csv trigger
+import wikipedia
+from nltk import tokenize #wiki sentences
+
 from sqlalchemy import create_engine, Table, MetaData
-from sqlalchemy import text as sqlalctext #edit st 2023-03-07#
+from sqlalchemy import text as sqlalctext #edit st 2023-03-07
 
 # from vis_functions import *
 
@@ -977,9 +981,9 @@ def backgroundworker_gdelt_csv_trigger(gdelt_text, response_url, channel_id):
         df = pd.merge(df_count[['Date','articles']],df_TimelineVolInfo, how='left', on=['Date'])
     
         # Save dataframe to csv
-        df.to_csv(os.path.expanduser("gdelt_file.csv"),index=False) # updated filename and directory edit mar 15 2023
+        df.to_csv((f"{gdelt_text}.csv"), index=False) # updated filename and directory edit mar 15 2023
         
-        return
+        return 'csv completed'
     
     # using the function defined above to produce csv
     # we are passing in the key obtained from the slash command in the function
@@ -994,7 +998,7 @@ def backgroundworker_gdelt_csv_trigger(gdelt_text, response_url, channel_id):
     container_name = "gdelt"
     blob_service_client = BlobServiceClient.from_connection_string (container_string) 
     container_client = blob_service_client.get_container_client(container_name)
-    filename = "gdelt_file.csv"
+    filename = f"{gdelt_text}.csv"
     blob_client = container_client.get_blob_client(filename)
     blob_name= filename
     with open(filename, "rb") as data:
@@ -1017,8 +1021,8 @@ def backgroundworker_gdelt_csv_trigger(gdelt_text, response_url, channel_id):
         assert response["file"]  # the uploaded file
         # Delete the blob
 
-        blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
-        blob_client.delete_blob()
+        # blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
+        # blob_client.delete_blob()
         
     except SlackApiError as e:
         # You will get a SlackApiError if "ok" is False
@@ -1034,12 +1038,12 @@ def backgroundworker_gdelt_csv_trigger(gdelt_text, response_url, channel_id):
 @app.route('/gdelt_csv_trigger', methods=['POST'])
 def gdelt_csv_trigger():
     data = request.form
-    #we are usging data2 to parse the information
+    # we are not usging data2 to parse the information in this case
     data2 = request.form.to_dict()
     #print(data)
     user_id = data.get('user_id')
     channel_id = data.get('channel_id')
-    #getting language to form wordclouds from
+    #getting keyword from user input and storing them into gdelt_text variable
     gdelt_text = data.get('text')
     
     response_url = data.get("response_url")
@@ -1058,6 +1062,141 @@ def gdelt_csv_trigger():
     #translate to and text to translate
     thr = Thread(target=backgroundworker_gdelt_csv_trigger, 
                  args=[gdelt_text,
+                       response_url,
+                       channel_id]
+                 )
+    
+    thr.start()
+    
+    #returning empty string with 200 response
+    return f'{greeting_message}', 200
+#######################################################__________________________________
+
+#################################### Note: ##############################################
+# WIKI_CSV_TRIGGER uses parameter names wordcloud_lang_to, wordcloud_lang_kw which are 
+# unrelated to wordcloud features
+# they are essentially wiki_csv_lang_to, and wiki_csv_keyword
+#########################################################################################
+#background worker for creating csv from wikipedia api
+def backgroundworker_wiki_csv_trigger(wordcloud_lang_to, wordcloud_lang_kw, response_url, channel_id):
+
+    # your task
+    def wikisentences(key,geo):
+        """ Get Wikipedia Raw Text for Specific Keyword in Specific Language (https://en.wikipedia.org/wiki/List_of_Wikipedias#Lists)
+        
+        Args:
+            key (str): Keyword
+            geo (str): Geo WP Code ('de')
+        
+        Returns:
+            df: Dataframe with Sentences from Content of Wiki Site
+        """
+        # Set Wikipedia language to geo
+        wikipedia.set_lang(geo)
+        # Get all suggested results for the query of key in wiki
+        all_results = wikipedia.search(key) 
+        # Select the first suggested result
+        key_original = all_results[0]
+        # Get the resulting wikipedia page for key_original
+        result = wikipedia.page(key_original, auto_suggest=False)
+        # Get the Content of the result
+        content_raw = result.content
+        # Split content_raw into sentences
+        sentences = tokenize.sent_tokenize(content_raw)
+        # Put the sentences into a dataframe
+        df = pd.DataFrame(data={'text': sentences})
+        
+        return df
+
+    
+    # Generate csv from wikipedia keyword and language pair
+    wikisentences(wordcloud_lang_kw, wordcloud_lang_to).to_csv('wiki_sentences.csv', 
+                                                               index_label='index') #column name is set to index
+    
+    
+    #payload is required to to send second message after task is completed
+    payload = {"text":"your task is complete",
+                "username": "bot"}
+    
+    #uploading the file to azure blob storage
+    container_string=os.environ["CONNECTION_STRING"]
+    storage_account_name = "storage4slack"
+    container_name = "wikicsv"
+    blob_service_client = BlobServiceClient.from_connection_string (container_string) 
+    container_client = blob_service_client.get_container_client(container_name)
+    filename = 'wiki_sentences.csv'
+    blob_client = container_client.get_blob_client(filename)
+    blob_name= filename
+    with open(filename, "rb") as data:
+        blob_client.upload_blob(data)
+        
+    try:
+        # Download the blob as binary data
+        blob_client = blob_service_client.get_blob_client(container_name, blob_name)
+        blob_data = blob_client.download_blob().readall()
+        
+        # Open the audio file and read its contents
+        with open(filename, 'rb') as file:
+            file_data = file.read()
+            
+        
+        #         filename=f"{(text[:3]+text[-3:])}.mp3"
+        response = client.files_upload(channels=channel_id,
+                                        file=file_data,
+                                        initial_comment=f"CSV generated for language-keyword: \n{wordcloud_lang_to.upper()} *{wordcloud_lang_kw.title()}*: ")
+        assert response["file"]  # the uploaded file
+        # Delete the blob
+
+        blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
+        blob_client.delete_blob()
+        
+    except SlackApiError as e:
+        # You will get a SlackApiError if "ok" is False
+        assert e.response["ok"] is False
+        assert e.response["error"]  # str like 'invalid_auth', 'channel_not_found'
+        print(f"Got an error: {e.response['error']}")
+
+    requests.post(response_url,data=json.dumps(payload))
+#######################################################__________________________________
+
+#################################### Note: ##############################################
+# WIKI_CSV_TRIGGER uses parameter names wordcloud_lang_to, wordcloud_lang_kw which are 
+# unrelated to wordcloud features
+# they are essentially wiki_csv_lang_to, and wiki_csv_keyword
+#########################################################################################
+#wiki_csv trigger slash command which creates csv from Wikipedia API 
+#and posts to slack
+@app.route('/wiki_csv_trigger', methods=['POST'])
+def wiki_csv_trigger():
+    data = request.form
+    #we are usging data2 to parse the information
+    data2 = request.form.to_dict()
+    #print(data)
+    user_id = data.get('user_id')
+    channel_id = data.get('channel_id')
+    #getting language to for wiki csv trigger
+    wordcloud_lang_to = data.get('text').split()[0].lower()
+    # obtaining the keyword anything after the 3rd letter
+    # because example: en "keyword"
+    wordcloud_lang_kw = data.get('text')[3:]
+    
+    response_url = data.get("response_url")
+    #event = payload.get('event', {})
+    #text = event.get('text')
+    greeting_message = "Processing your request. Please wait."
+    ending_message = "Process executed successfully"
+
+    
+    client.chat_postMessage(channel=channel_id, # updated to channel_id edit: mar 15, 2023
+                            text="CSV loading. Please wait."
+                            )
+    
+    
+    #triggering backgroundworker for deepl with arguments lang to translate from
+    #translate to and text to translate
+    thr = Thread(target=backgroundworker_wiki_csv_trigger, 
+                 args=[wordcloud_lang_to,
+                       wordcloud_lang_kw,
                        response_url,
                        channel_id]
                  )
