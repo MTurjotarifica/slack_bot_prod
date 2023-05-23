cc074efa7963ff0987a4bc85346b1a8c8b68adba
MTurjotarifica <116021103+MTurjotarifica@users.noreply.github.com>
2023-05-24 02:43:02 +0800

    Update app.py

--- app.py
+++ app.py
@@ -55,6 +55,7 @@ from sqlalchemy import text as sqlalctext #edit st 2023-03-07
 
 # Functions to import 
 from Imports.importFunction import *
+from Routes.mp3_route import *
 
 load_dotenv()
 
@@ -236,33 +237,8 @@ def interactive_trigger_route(client):
 #########################################################################################
 # mp3 trigger slash command which creates mp3 and posts to slack
 @app.route('/mp3_trigger', methods=['POST'])
-def mp3_trigger():
-    data = request.form
-    #we are usging data2 to parse the information
-    data2 = request.form.to_dict()
-    #print(data)
-    user_id = data.get('user_id')
-    channel_id = data.get('channel_id')
-    text = data.get('text')
-    response_url = data.get("response_url")
-    #event = payload.get('event', {})
-    #text = event.get('text')
-    greeting_message = "Processing your request. Please wait."
-    ending_message = "Process executed successfully"
-
-
-    client.chat_postMessage(channel=channel_id,
-                            text="MP3 loading. Please wait."
-                            )
-
-
-    # triggering backgroundworker1 
-    thr = Thread(target=backgroundworker_mp3, args=[client, text, response_url, channel_id])
-    thr.start()
-
-
-    # returning empty string with 200 response
-    return f'{greeting_message}', 200
+def mp3_route():
+    return mp3_trigger(client)
 #######################################################__________________________________
 
 
