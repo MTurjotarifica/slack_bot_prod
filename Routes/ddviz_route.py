0f6dab0e27976ef0ed4b437af9236e898c797cc7
MTurjotarifica <116021103+MTurjotarifica@users.noreply.github.com>
2023-05-24 16:43:38 -0400

    Add files via upload

---
+++ Routes/ddviz_route.py
@@ -0,0 +1,15 @@
+from ..Blocks.blocks import *
+from flask import Flask, request, make_response
+
+def dd_vis_trigger(client):
+    data = request.form
+    channel_id = data.get('channel_id')
+
+    client.chat_postMessage(channel=channel_id, 
+                                        text="Visualization:  ",
+                                        blocks = dd_vis_trigger_block
+                                        )
+
+
+    #returning empty string with 200 response
+    return 'dd_vis trigger works', 200
---
+++ Routes/wiki_route.py
@@ -0,0 +1,45 @@
+from BackgroundWorkers.wiki_csv import *
+from flask import Flask, request, make_response
+from threading import Thread
+
+def wiki_csv_trigger(client):
+    data = request.form
+    #we are usging data2 to parse the information
+    data2 = request.form.to_dict()
+    #print(data)
+    user_id = data.get('user_id')
+    channel_id = data.get('channel_id')
+    #getting language to for wiki csv trigger
+    wordcloud_lang_to = data.get('text').split()[0].lower()
+    # obtaining the keyword anything after the 3rd letter
+    # because example: en "keyword"
+    wordcloud_lang_kw = data.get('text')[3:]
+    
+    response_url = data.get("response_url")
+    #event = payload.get('event', {})
+    #text = event.get('text')
+    greeting_message = "Processing your request. Please wait."
+    ending_message = "Process executed successfully"
+
+    
+    client.chat_postMessage(channel=channel_id, # updated to channel_id edit: mar 15, 2023
+                            text="CSV loading. Please wait."
+                            )
+    
+    
+    #triggering backgroundworker for deepl with arguments lang to translate from
+    #translate to and text to translate
+    thr = Thread(target=backgroundworker_wiki_csv_trigger, 
+                 args=[client,
+                       wordcloud_lang_to,
+                       wordcloud_lang_kw,
+                       response_url,
+                       channel_id,
+                       ]
+                 )
+    
+    thr.start()
+    
+    #returning empty string with 200 response
+    return f'{greeting_message}', 200
+#######################################################__________________________________
