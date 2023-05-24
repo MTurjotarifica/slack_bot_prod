b1696dae434d0e043d256111d005770d55f8f31a
MTurjotarifica <116021103+MTurjotarifica@users.noreply.github.com>
2023-05-25 04:43:58 +0800

    Update importFunction.py

--- Imports/importFunction.py
+++ Imports/importFunction.py
@@ -3,13 +3,13 @@ from Blocks.blocks import *
 
 from Database.database import *
 
-from BackgroundWorkers.vis_functions import *
-from BackgroundWorkers.wiki_csv import *
-from BackgroundWorkers.wordcloud_slack import *
-from BackgroundWorkers.ddviz import *
-from BackgroundWorkers.gdelt import *
-from BackgroundWorkers.mp3 import *
-from BackgroundWorkers.deepl import *
+from Routes.BackgroundWorkers.vis_functions import *
+from Routes.BackgroundWorkers.wiki_csv import *
+from Routes.BackgroundWorkers.wordcloud_slack import *
+from Routes.BackgroundWorkers.ddviz import *
+from Routes.BackgroundWorkers.gdelt import *
+from Routes.BackgroundWorkers.mp3 import *
+from Routes.BackgroundWorkers.deepl import *
 
 from Routes.mp3_route import *
 from Routes.deepl_route import *
