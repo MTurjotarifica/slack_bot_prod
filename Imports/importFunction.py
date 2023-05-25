0390676f7f6688e45c6b2acc084ec4d5bdc9f9e3
MTurjotarifica <116021103+MTurjotarifica@users.noreply.github.com>
2023-05-26 04:35:32 +0800

    Update importFunction.py

--- Imports/importFunction.py
+++ Imports/importFunction.py
@@ -3,7 +3,7 @@ from Blocks.blocks import *
 
 from Database.database import *
 
-from BackgroundWorkers.vis_functions import *
+from BackgroundWorkers.vis.vis_functions import *
 from BackgroundWorkers.wiki_csv import *
 from BackgroundWorkers.wordcloud_slack import *
 from BackgroundWorkers.ddviz import *
